cc3ef1abf8c330bd66ee2aa7b55e2a24e24695ab
ZouJian <chsword@qq.com>
2014-05-23 00:17:50 +0800

    111

---
+++ AmTools/AmTools.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AmTools.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var p = 2;
+            var ret = MyClass.A(p);
+            //Assert.AreEqual(3, ret);
+            Console.WriteLine(ret);
+        }
+    }
+    class MyClass
+    {
+        public static int A(int a) { return a +2; }
+
+    }
+}
