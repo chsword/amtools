45ad7804f996561a2103e41969bcafa7d6bd5720
ZouJian <chsword@qq.com>
2014-05-26 23:33:19 +0800

    addwebkit

---
+++ AmTools/AmTools/Extensions/HtmlNodeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmTools
+{
+    static class HtmlNodeExtensions
+    {
+        public static bool HasClass(this HtmlNode node,string className) {
+            if (node.Attributes["class"] == null) return false;
+            var @class = (
+                (node.Attributes["class"]
+                ).Value ?? "").Split(' ');
+            return @class.Contains(className);
+        }
+    }
+    static class HtmlNodeCollectionExtensions
+    {
+        public static HtmlNode FindWithClass(this HtmlNodeCollection nodes, string className)
+        {
+            return nodes.FirstOrDefault(c => c.HasClass(className));
+        }
+    }
+}
